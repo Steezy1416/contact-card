f770b9746008a60b5e62bb2b64a84bcd118f422f
alexfortin <fortin.alex14@gmail.com>
2022-12-22 10:41:12 -0600

    added service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const WorkboxPlugin = require("workbox-webpack-plugin")
+const {InjectManifest} = require("workbox-webpack-plugin")
 
 module.exports = {
     mode: "production",
@@ -12,6 +14,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./index.html",
             title: "Webpack Plugin"
+        }),
+        new InjectManifest({
+            swSrc: "./src/sw.js",
+            swDest: "service-worker.js"
         })
     ],
     module: {
